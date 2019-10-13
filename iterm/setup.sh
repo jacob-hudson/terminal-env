d2db99e89f321faafd0779c784c2d5665e85d67b
Jacob A. Hudson <jacob.alan.hudson@gmail.com>
2019-10-12 21:09:14 -0500

    setup script

---
+++ iterm/setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+git clone https://github.com/powerline/fonts.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /Users/jacob/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions /Users/jacob/.oh-my-zsh/custom/plugins/zsh-autosuggestions
